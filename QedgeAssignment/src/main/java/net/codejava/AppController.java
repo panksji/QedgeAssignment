46e43dbd8b9f488cead30f95bdc83ca2499317be
pansharm1 <pansharm1@WKWIN8336075.global.publicisgroupe.net>
2020-10-28 12:07:54 +0530

    First commit

---
+++ QedgeAssignment/src/main/java/net/codejava/AppController.java
@@ -0,0 +1,75 @@
+package net.codejava;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AppController {
+
+	@Autowired
+	private ProductService service; 
+	
+	
+	@RequestMapping("/")
+	public String viewHomePage(Model model) {
+		List<Product> listProducts = service.listAll();
+		model.addAttribute("listProducts", listProducts);
+		
+		return "index";
+	}
+	
+	@RequestMapping("/new")
+	public String showNewProductPage(Model model) {
+		Product product = new Product();
+		model.addAttribute("product", product);
+		
+		return "new_product";
+	}
+	
+	@RequestMapping(value="/logout", method=RequestMethod.GET)
+	 public String logoutPage(HttpServletRequest request, HttpServletResponse response) {  
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();  
+        if (auth != null){      
+           new SecurityContextLogoutHandler().logout(request, response, auth);  
+        }  
+         return "redirect:/login";  
+     }  
+	
+	
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveProduct(@ModelAttribute("product") Product product) {
+		service.save(product);
+		
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/edit/{id}")
+	public ModelAndView showEditProductPage(@PathVariable(name = "id") int id) {
+		ModelAndView mav = new ModelAndView("edit_product");
+		Product product = service.get(id);
+		mav.addObject("product", product);
+		
+		return mav;
+	}
+	
+	@RequestMapping("/delete/{id}")
+	public String deleteProduct(@PathVariable(name = "id") int id) {
+		service.delete(id);
+		return "redirect:/";		
+	}
+}
---
+++ QedgeAssignment/target/classes/templates/logout-form.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<h2>Spring Security Example</h2>
+ <p>Time: ${time}</p>
+  <form action="/logout" method="post">
+     <input type="hidden"
+            name="${_csrf.parameterName}"
+            value="${_csrf.token}"/>
+  <input type="submit" value="Logout">
+</form>
+
+</body>
+</html>
